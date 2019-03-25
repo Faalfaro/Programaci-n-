2efc319d273e8a05352ab48d3b2f021fc9ea2d04
Fernando Alfaro <30763824+Faalfaro@users.noreply.github.com>
2019-03-25 16:20:53 -0300

    Update primero.c

--- src/primero/primero.c
+++ src/primero/primero.c
@@ -1,1 +1,13 @@
-printf("hola %s","fenia")
+
+// Importa el módulo standard de Input / Output
+#include <stdio.h>
+
+// Funcion que se llama al ejecutar el programa
+int main()
+{
+  // Imprime en la consola
+  printf("Hello, Fenia!\n");
+
+  // Todo salió bien
+  return 0;
+}
